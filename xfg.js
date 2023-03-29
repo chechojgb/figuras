1ba8e6ce20f2432059118ab0c4353635bb414425
chechojgb <100326731+chechojgb@users.noreply.github.com>
2023-03-28 21:26:15 -0500

    commit initial

---
+++ xfg.js
@@ -0,0 +1,37 @@
+// <!-- Hecho por sergio ortiz garzon 
+function circulo() {
+    var figura = document.getElementById("figura");
+    figura.classList.toggle("circulo");
+}
+function rectangulo(){
+    var figura = document.getElementById("figura");
+    figura.classList.toggle("rectangulo");
+}
+function rombo(){
+    var figura = document.getElementById("figura");
+    figura.classList.toggle("rombo");
+}
+function gif(){
+    var figura = document.getElementById("figura");
+    figura.classList.toggle("gif");
+}
+function izquierda(){
+    var figura = document.getElementById("figura");
+    figura.classList.toggle("izquierda");
+}
+function derecha(){
+    var figura = document.getElementById("figura");
+    figura.classList.toggle("derecha");
+}
+function arriba(){
+    var figura = document.getElementById("figura");
+    figura.classList.toggle("arriba");
+}
+function abajo(){
+    var figura = document.getElementById("figura");
+    figura.classList.toggle("abajo");
+}
+function diagonal(){
+    var figura = document.getElementById("figura");
+    figura.classList.toggle("diagonal");
+}
